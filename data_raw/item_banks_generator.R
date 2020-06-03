f39b7b225871a1601763579df73a9e7671b63df2
Frank Höger <frank.hoeger@mirsem.org>
2020-06-03 13:15:38 +0200

    rename generators

--- data_raw/item_banks_generator.R
+++
@@ -1,6 +0,0 @@
-for (questionnaire in c("MRT", "PAT", "PFT", "SRT")) {
-    item_bank_raw <- read.csv(paste0("data_raw/item_banks/", questionnaire, "_item_bank.csv"), sep = ";", stringsAsFactors = FALSE, header = TRUE)
-    assign(paste0(questionnaire, "_item_bank"), item_bank_raw)
-}
-
-usethis::use_data(MRT_item_bank, PAT_item_bank, PFT_item_bank, SRT_item_bank, overwrite = TRUE)
