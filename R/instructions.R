69cd3964337c5d758073a941e9086fe8c63a626e
Frank Höger <frank.hoeger@mirsem.org>
2020-06-08 12:51:27 +0200

    make task images responsive for PAT and PFT

--- R/instructions.R
+++ R/instructions.R
@@ -1,8 +1,8 @@
 instructions_page <- function(key, label, style = "text-align: center; margin-left: 10%; margin-right: 10%; margin-bottom: 20px;") {
   ui <- shiny::div(
     shiny::div(psychTestR::i18n(key, html = TRUE)),
-    if (label == "PFT") shiny::tags$img(src = sprintf("www/images/%s/example.png", label), style = "margin-top: 10px; width: 468px; display: block;"),
-    if (label == "PFT") shiny::tags$img(src = sprintf("www/images/%s/example_folding.png", label), style = "margin-top: 10px; margin-bottom: 20px; width: 468px; display: block;"),
+    if (label == "PFT") shiny::tags$img(src = sprintf("www/images/%s/example.png", label), style = "margin-top: 10px; width: 100%; height: auto;"),
+    if (label == "PFT") shiny::tags$img(src = sprintf("www/images/%s/example_folding.png", label), style = "margin-top: 10px; margin-bottom: 20px; width: 100%; height: auto;"),
     shiny::p(psychTestR::trigger_button("next", psychTestR::i18n("CONTINUE"), style = "margin-top: 15px"))
   )
 
